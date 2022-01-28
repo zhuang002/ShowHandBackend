902f0aeb3a3b455795382baf825706c3189496e5
zhuang002 <zhuang001@gmail.com>
2022-01-27 23:47:51 -0500

    v1

--- src/ShowHandLogic.java
+++ src/ShowHandLogic.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -39,14 +40,13 @@ public class ShowHandLogic implements Service {
 	@Override
 	public void newGame() {
 		// TODO Auto-generated method stub
-		for (int i=0;i<bets.length;i++) {
-			bets[i] = 0;
-		}
+		
 		
 		for (int i=0;i<inventories.length;i++) {
 			inventories[i] = initialInventory;
+			
 		}
-		newRound();
+
 	}
 
 	@Override
@@ -70,6 +70,14 @@ public class ShowHandLogic implements Service {
 		}
 		shuffleCards();
 		
+		for (int i=0;i<bets.length;i++) {
+			bets[i] = 0;
+			this.guiController.displayBets(i, bets[i]);
+		}
+		
+		for (int i=0;i<inventories.length;i++) {
+			this.guiController.displayInventories(i, inventories[i]);
+		}
 		
 	}
 
@@ -93,23 +101,47 @@ public class ShowHandLogic implements Service {
 		Card card = this.drawCards.get(0);
 		this.playersCards[player].add(card);
 		this.drawCards.remove(0);
-		this.guiController.displayCards(player, (Card[])this.playersCards[player].toArray());
+		this.guiController.displayCards(player, this.playersCards[player]);
 		return card;
 	}
 
 	@Override
-	public void bet(int amount, int player) {
+	public void bet(int player, int amount) {
 		// TODO Auto-generated method stub
 		bets[player] = bets[player]+amount;
+		inventories[player] -= amount;
+		this.guiController.displayBets(player, bets[player]);
+		this.guiController.displayInventories(player, inventories[player]);
+		
 	}
 
 	@Override
 	public void giveUp(int player) {
 		// TODO Auto-generated method stub
-		this.inventories[player] -= this.bets[player];
-		this.inventories[0] += this.bets[player];
-		this.bets[player]=0;
-		
+		this.playersDone[player]=true;
+		if (player!=0) {
+			
+			this.inventories[0] += this.bets[player];
+			this.bets[player]=0;
+			
+			this.done(player);
+		} else {
+			for (int i=1;i<noOfPlayers;i++) {
+				inventories[i]+=bets[i]*2;
+				inventories[0]-=bets[i];
+				bets[i]=0;
+			}
+			
+			for (int i=0;i<noOfPlayers;i++) {
+				this.guiController.displayInventories(i, inventories[i]);
+			}
+			this.guiController.roundDone();
+			for (int i=0;i<inventories.length;i++) {
+				if (inventories[i] <= 0) {
+					this.guiController.gameOver(i);
+				}
+			}
+		}
 	}
 
 	@Override
@@ -121,6 +153,7 @@ public class ShowHandLogic implements Service {
 		for (int i=0;i<this.playersDone.length;i++) {
 			if (this.playersDone[(i+player)%noOfPlayers]==false) {
 				allDone = false;
+				nextPlayer = (i+player)%noOfPlayers;
 				break;
 			}
 		}
@@ -129,14 +162,77 @@ public class ShowHandLogic implements Service {
 			completeRound();
 		} else {
 			if (nextPlayer == 0) {
-				ShowHandStrategy.getInstance().requireDraw(this.playersCards[nextPlayer]);
+				while (!this.playersDone[nextPlayer])
+					ShowHandStrategy.getInstance().requireDraw(this.playersCards[nextPlayer]);
 			}
 		}
 		
 	}
 
+	private void completeRound() {
+		// TODO Auto-generated method stub
+		for (int i=0;i<this.playersCards.length;i++) {
+			List<Card> cards = this.playersCards[i];
+			this.guiController.displayCards(i, cards);
+		}
+
+		int hostPoints = getMaxPoints(this.playersCards[0]);
+		inventories[0]+=bets[0];
+		
+		for (int i=1;i<noOfPlayers;i++) {
+			int points = getMaxPoints(this.playersCards[i]);
+			if (hostPoints<points) {
+				this.inventories[i]+=this.bets[i]*2;
+				this.inventories[0]-=this.bets[i];
+			} else {
+				this.inventories[0]+=this.bets[i];
+			}
+		}
+		for (int i=0;i<noOfPlayers;i++) {
+			this.guiController.displayInventories(i, inventories[i]);
+		}
+		this.guiController.roundDone();
+		for (int i=0;i<inventories.length;i++) {
+			if (inventories[i] <= 0) {
+				this.guiController.gameOver(i);
+			}
+		}
+		
+	}
+
+	private int getMaxPoints(List<Card> cards) {
+		// TODO Auto-generated method stub
+		List<Integer> possiblePoints = getPossiblePoints(cards);
+		if (possiblePoints.isEmpty()) return 0;
+		return possiblePoints.get(0);
+	}
+
 	public GuiController getGuiController() {
 		// TODO Auto-generated method stub
 		return this.guiController;
+	}
+	
+	public List<Integer> getPossiblePoints(List<Card> cards) {
+		// TODO Auto-generated method stub
+		List<Integer> pointsList = new ArrayList<>();
+		int A_s = 0;
+		int points = 0;
+		for (Card card:cards) {
+			if (card.value == 1) {
+				A_s++;
+			} else if (card.value<10) {
+				points+=card.value;
+			} else {
+				points+=10;
+			}
+		}
+		
+		for (int i=0;i<=A_s;i++) {
+			int p = points+i+(A_s-i)*11;
+			if (p<=21)
+				pointsList.add(p);
+		}
+		Collections.sort(pointsList,Collections.reverseOrder());
+		return pointsList;
 	}
 }
